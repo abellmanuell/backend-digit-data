2ac6189101b22fa394b5da0d0fa47a0ee6943e68
abellmanuell <mannydev02@gmail.com>
2025-03-14 00:24:11 +0100

    Testing... testing... testing...

--- routes/oauth/oauth.js
+++ routes/oauth/oauth.js
@@ -10,62 +10,62 @@ const { server_response } = require("../../utils/server_response");
 const { generateToken } = require("../../utils/JwtVerify/jwtVerify");
 require("dotenv").config();
 
-async function getUserData({
-  access_token,
-  token_type,
-  refresh_token,
-  expiry_date,
-  res,
-}) {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
-  );
-  const { sub, email } = await response.json();
-  const existingUser = await findUser(email);
-
-  /* Check whether user exist */
-  if (existingUser) {
-    const isGoogleIdExist = Object.hasOwn(existingUser, "google_id");
-
-    /* Check if user has GOOGLE_ID */
-    if (isGoogleIdExist) {
-      const token = generateToken(existingUser);
-      return server_response(200, res, "Sign in successfully!", {
-        token,
-      });
-    } else {
-      return server_response(
-        403,
-        res,
-        "Oops! You previously signed up with your email and password. Please sign in using your email and password instead of Google."
-      );
-    }
-  }
-
-  const user = {
-    access_token,
-    token_type,
-    refresh_token,
-    expiry_date,
-    sub,
-    email,
-  };
-
-  const userCreated = await createUser(user);
-
-  if (userCreated.acknowledged) {
-    const existingUser =
-      userCreated.insertedId && (await findUserById(userCreated.insertedId));
-    const token = generateToken(existingUser);
-
-    return server_response(200, res, "Sign in successfully!", {
-      token,
-    });
-  }
-}
-
 router.get("/", async (req, res) => {
   try {
+    async function getUserData({
+      access_token,
+      token_type,
+      refresh_token,
+      expiry_date,
+    }) {
+      const response = await fetch(
+        `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+      );
+      const { sub, email } = await response.json();
+      const existingUser = await findUser(email);
+
+      /* Check whether user exist */
+      if (existingUser) {
+        const isGoogleIdExist = Object.hasOwn(existingUser, "google_id");
+
+        /* Check if user has GOOGLE_ID */
+        if (isGoogleIdExist) {
+          const token = generateToken(existingUser);
+          return server_response(200, res, "Sign in successfully!", {
+            token,
+          });
+        } else {
+          return server_response(
+            403,
+            res,
+            "Oops! You previously signed up with your email and password. Please sign in using your email and password instead of Google."
+          );
+        }
+      }
+
+      const newUser = {
+        access_token,
+        token_type,
+        refresh_token,
+        expiry_date,
+        sub,
+        email,
+      };
+
+      const userCreated = await createUser(newUser);
+
+      if (userCreated.acknowledged) {
+        const existingUser =
+          userCreated.insertedId &&
+          (await findUserById(userCreated.insertedId));
+        const token = generateToken(existingUser);
+
+        return server_response(200, res, "Sign in successfully!", {
+          token,
+        });
+      }
+    }
+
     const code = req.query.code;
 
     const redirectUrl = `${process.env.REDIRECT_URL}/googleme`;
@@ -77,11 +77,11 @@ router.get("/", async (req, res) => {
     );
 
     /* Get credentials */
-    const res = await oAuth2Client.getToken(code);
-    await oAuth2Client.setCredentials(res.tokens);
+    const authResponse = await oAuth2Client.getToken(code);
+    await oAuth2Client.setCredentials(authResponse.tokens);
     const credentials = oAuth2Client.credentials;
 
-    await getUserData({ ...credentials, res });
+    await getUserData(credentials);
   } catch (err) {
     console.log(err);
 
